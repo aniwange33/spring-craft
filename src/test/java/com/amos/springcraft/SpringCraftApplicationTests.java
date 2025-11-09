e09523e33117b5b75df6170fc964ba0dd5fe4781
tertese Amos <terteseamos@gmail.com>
2025-11-09 12:31:38 +0800

    feat: first commit

---
+++ src/test/java/com/amos/springcraft/SpringCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amos.springcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
